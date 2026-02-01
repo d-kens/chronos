56835fa2bb03459b19495c973f718b36917da5e0
dickensomondionyango <d.onyango@carepay.com>
2026-02-01 20:52:01 +0300

    update project title

--- src/app.module.ts
+++ src/app.module.ts
@@ -15,7 +15,7 @@ import { TimetableModule } from './timetable/timetable.module';
       host: 'localhost',
       port: 3306,
       username: 'root',
-      password: 'yourpass',
+      password: 'your password',
       database: 'timetable_db',
       entities: [Activity, ActivitySession],
       synchronize: true,
